8ff34146cbd854b35783df7b5583568c56198fea
santimontironi <santiimontironi@gmail.com>
2026-01-13 19:28:53 -0300

    cambio en el transporter

--- src/app/api/messageContact/route.js
+++ src/app/api/messageContact/route.js
@@ -3,8 +3,8 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
-    port: Number(process.env.EMAIL_PORT),
-    secure: true,
+    port: 587, 
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
